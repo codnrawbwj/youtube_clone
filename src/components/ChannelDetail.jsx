e0460a36544ae0379bce32efb3ed76698b1e9904
Chae-uk <codnrawbwj@gmail.com>
2023-06-02 14:04:01 -0500

    homepage

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`search?channelID=${id}&part=snippet&order=date`)
+    .then((data) => setVideos(data?.items));
+  }, [id])
+
+  console.log(channelDetail, videos);
+
   return (
-    <div>ChannelDetail</div>
+    <div>
+      {id}
+    </div>
   )
 }
 
